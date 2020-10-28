f78ba5359b6527c331c235e632eaee6b94d92f61
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2020-10-28 22:26:17 +0100

    Instaling and testing bulma in project. Rewriting main nav for using bulma css classes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand is-text">
+      DeckBuilder
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <router-link 
+          to="/"
+          class="navbar-item"
+        >
+          Home
+        </router-link>
+        <router-link 
+          to="/about"
+          class="navbar-item"
+        >
+          About
+        </router-link>
+      </div>
+    </div>
+  </nav>
+
   <router-view />
+
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
+
+  .navbar-brand.is-text {
+    font-family: 'Times New Roman', Times, serif;
+    line-height: 2;
+    font-size: 1.5rem;
+    margin: 0 .5em
+  }
+
 </style>
