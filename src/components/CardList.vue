a9d244b1696461a1c1e345b3a696ba4f74e84d25
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2020-11-01 16:37:31 +0100

    Typed store, using Options decorator on CardList component to render counter stored in Vuex

---
+++ src/components/CardList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="c-card-list">
+    {{ counter }}
+  </ul>
+</template>
+
+<script lang="ts">
+import { ActionTypes } from '@/store/action-types';
+import { Options, Vue } from "vue-class-component";
+import { mapActions, mapState } from 'vuex';
+
+@Options<CardList>({
+  computed: {
+    ...mapState({
+      counter: (state: any) => state.counter
+    })
+  },
+
+  methods: {
+    ...mapActions([
+      ActionTypes.GET_COUNTER
+    ])
+  }
+})
+export default class CardList extends Vue {
+  // mounted() {
+    
+  // }
+}
+</script>
+
+<style lang="scss">
+.c-card-list {
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <div class="card-list">
+            <CardList />
+          </div>
+        </div>
+        <div class="column is-one-fifth">mid</div>
+        <div class="column">
+          <div class="card-list">
+            <CardList />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import CardList from "@/components/CardList.vue";
 
 @Options({
   components: {
-    HelloWorld
+    HelloWorld,
+    CardList
   }
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss">
+.card-list {
+  border: 1px solid #999999;
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 0;
+}
+</style>
